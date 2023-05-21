5a03c615ada868aecd5b043e852a1308e1b29ac1
AdityaSurve <surveaditya521@gmail.com>
2023-05-21 23:33:53 +0530

    bishopComplete

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -120,6 +120,33 @@ export default class Referee {
         if (this.isEmptyOrOccupiedByOpponent(desiredPosition, team, boardState))
           return true;
       }
+    } else if (type === PieceType.Rook) {
+      if (
+        desiredPosition.x === initialPosition.x ||
+        desiredPosition.y === initialPosition.y
+      ) {
+        const xDirection =
+          desiredPosition.x - initialPosition.x === 0
+            ? 0
+            : desiredPosition.x - initialPosition.x > 0
+            ? 1
+            : -1;
+        const yDirection =
+          desiredPosition.y - initialPosition.y === 0
+            ? 0
+            : desiredPosition.y - initialPosition.y > 0
+            ? 1
+            : -1;
+        let x = initialPosition.x + xDirection;
+        let y = initialPosition.y + yDirection;
+        while (x !== desiredPosition.x || y !== desiredPosition.y) {
+          if (this.isOccupied({ x, y }, boardState)) return false;
+          x += xDirection;
+          y += yDirection;
+        }
+        if (this.isEmptyOrOccupiedByOpponent(desiredPosition, team, boardState))
+          return true;
+      }
     }
     return false;
   }
